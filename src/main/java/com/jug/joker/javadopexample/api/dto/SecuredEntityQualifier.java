2a17af083388751dd27a26b8cbaedcab79e0d9aa
Felix <vip.zatebe@mail.ru>
2024-06-29 01:30:35 +0300

    Lombok constant generation

--- src/main/java/com/jug/joker/javadopexample/api/dto/SecuredEntityQualifier.java
+++ src/main/java/com/jug/joker/javadopexample/api/dto/SecuredEntityQualifier.java
@@ -1,18 +1,15 @@
 package com.jug.joker.javadopexample.api.dto;
 
+import lombok.experimental.FieldNameConstants;
+
+@FieldNameConstants(onlyExplicitlyIncluded = true)
 public enum SecuredEntityQualifier {
-    CUSTOMER(Constants.CUSTOMER),
-    PRODUCT(Constants.PRODUCT),
-    PRODUCT_PROPERTIES(Constants.PRODUCT_PROPERTIES),
-    PURCHASE(Constants.PURCHASE);
-
-    SecuredEntityQualifier(String qualifier) {
-    }
-
-    public static class Constants {
-        public static final String CUSTOMER = "CUSTOMER";
-        public static final String PRODUCT = "PRODUCT";
-        public static final String PURCHASE = "PURCHASE";
-        public static final String PRODUCT_PROPERTIES = "PRODUCT_PROPERTIES";
-    }
+    @FieldNameConstants.Include
+    CUSTOMER,
+    @FieldNameConstants.Include
+    PRODUCT,
+    @FieldNameConstants.Include
+    PRODUCT_PROPERTIES,
+    @FieldNameConstants.Include
+    PURCHASE;
 }
