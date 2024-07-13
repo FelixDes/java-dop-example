c57ce248e6659197536f8c8d44f98ece8707f864
felix <felixdesyatirikov@gmail.com>
2024-07-13 13:34:53 +0300

    Remove unused @JsonConstructor

--- src/main/java/com/jug/joker/javadopexample/api/dto/definition/PurchaseDefinition.java
+++ src/main/java/com/jug/joker/javadopexample/api/dto/definition/PurchaseDefinition.java
@@ -1,15 +1,14 @@
 package com.jug.joker.javadopexample.api.dto.definition;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.jug.joker.javadopexample.api.dto.SecuredEntityQualifier;
 
 public record PurchaseDefinition(
         Long id
 ) implements LongIdEntityDefinition {
-    @JsonCreator
     public PurchaseDefinition {
         if (id <= 0) throw new IllegalArgumentException("id must be positive");
     }
+
     @Override
     public SecuredEntityQualifier getQualifier() {
         return SecuredEntityQualifier.PURCHASE;
